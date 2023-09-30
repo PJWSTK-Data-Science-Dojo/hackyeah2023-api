ab6ee51980d2eb7c712d09ca8558de9506196f59
Pexo <contact@pexogamedev.pl>
2023-10-01 00:03:45 +0200

    Updated local dockerbuild bash

--- HackYeah-API/BuildAndRunDocker.bat
+++ HackYeah-API/BuildAndRunDocker.bat
@@ -2,4 +2,4 @@ docker stop hackyeah-api
 docker rm hackyeah-api
 docker rmi hackyeah-api
 docker build -t hackyeah-api .
-docker run -d -p 6969:80 --name hackyeah-api hackyeah-api
+docker run -d -p 5012:80 --name hackyeah-api hackyeah-api
