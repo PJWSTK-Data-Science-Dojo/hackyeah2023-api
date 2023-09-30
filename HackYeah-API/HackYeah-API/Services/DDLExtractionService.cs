5bbedfe7263e3dbba9a2088cf9fb22595bb930bb
Filip Dzieciol <48879563+AnsealArt@users.noreply.github.com>
2023-09-30 17:02:59 +0200

    Add table metadata gathering query

--- HackYeah-API/HackYeah-API/Services/DDLExtractionService.cs
+++ HackYeah-API/HackYeah-API/Services/DDLExtractionService.cs
@@ -1,4 +1,5 @@
 ﻿using HackYeah_API.Services.Interfaces;
+using System.Text;
 
 namespace HackYeah_API.Services;
 
@@ -19,9 +20,49 @@ public class DdlExtractionService : IDdlExtractionService
     {
         await SaveFile(sqlLiteFile);
 
-        //todo read sql file
-        string ddl = "null";
-        await _mlService.SendDDL(ddl);
+        string sql = @"
+                    SELECT
+                        m.name AS table_name,
+                        p.name AS column_name,
+                        p.[type] AS data_type,
+                        p.[notnull] AS not_null,
+                        p.[dflt_value] AS default_value,
+                        f.[table] AS referenced_table,
+                        f.[to] AS referenced_column
+                    FROM
+                        sqlite_master AS m
+                    JOIN
+                        pragma_table_info(m.name) AS p
+                    LEFT JOIN
+                        pragma_foreign_key_list(m.name) AS f
+                    ON
+                        p.name = f.'from'
+                    WHERE
+                        m.type = 'table'
+                    ORDER BY
+                        m.name, p.cid;
+                ";
+        var result = await _queryExecutor.ExecuteQueryAsync(sql);
+
+        if ( result.errorMessage is not null )
+        {
+            return result.errorMessage;
+        } 
+        else
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            foreach (var dict in result.result)
+            {
+                foreach (var kvp in dict)
+                {
+                    stringBuilder.Append($"{kvp.Key}: {kvp.Value}\n");
+                }
+                stringBuilder.Append("\n"); // Add a separator between dictionaries
+            }
+
+            await _mlService.SendDDL(stringBuilder.ToString());
+        }
+        
         return await Task.Run(() => "null");
     }
 
